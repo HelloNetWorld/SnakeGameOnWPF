51f9174bd597b99169abef260113690e6f7c748a
v-vasiliev <v-vasiliev@VASILIEV-VV>
2018-09-20 10:00:03 +0300

    Add project files.

---
+++ SimpleSnakeGame/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace SimpleSnakeGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += SnakeGameLoaded;
+        }
+
+        private void SnakeGameLoaded(object sender, RoutedEventArgs e)
+        {
+            var snakeGame = new SnakeGame
+            {
+                Snake = new Snake
+                {
+                    Length = 3,
+                    StartPosition = StartPosition.Center
+                },
+                GameField = new GameField
+                {
+                    SquareSize = 50,
+                    Field = playingField
+                },
+                Difficulty = Difficulty.Hard
+            };
+            snakeGame.ReadySteadyGo();
+        }
+    }
+}
